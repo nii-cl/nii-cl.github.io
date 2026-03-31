faa6f497e0a66ea26f26bcaf893e169dfb32015c
penzant <info@penzant.net>
2026-03-31 10:24:17 +0900

    add greedy nav bar

--- assets/js/common.js
+++ assets/js/common.js
@@ -57,4 +57,80 @@ $(document).ready(function () {
   $('[data-toggle="popover"]').popover({
     trigger: "hover",
   });
+
+  const $siteNav = $("#site-nav");
+
+  if ($siteNav.length) {
+    const $btn = $siteNav.find(".greedy-nav__toggle");
+    const $vlinks = $siteNav.find(".visible-links");
+    const $hlinks = $siteNav.find(".hidden-links");
+    const breakWidths = [];
+    let rafId = null;
+
+    const getAvailableSpace = () => {
+      const buttonWidth = $btn.hasClass("hidden") ? 0 : $btn.outerWidth(true);
+      return $siteNav.innerWidth() - buttonWidth - 1;
+    };
+
+    const getVisibleWidth = () => {
+      const visibleLinks = $vlinks.get(0);
+      return visibleLinks ? visibleLinks.scrollWidth : 0;
+    };
+
+    const closeHiddenLinks = () => {
+      $hlinks.addClass("hidden");
+      $btn.attr("aria-expanded", "false");
+    };
+
+    const updateNav = () => {
+      const availableSpace = getAvailableSpace();
+
+      if (getVisibleWidth() > availableSpace && $vlinks.children().length > 0) {
+        breakWidths.push(getVisibleWidth());
+        $vlinks.children().last().detach().prependTo($hlinks);
+        $btn.removeClass("hidden");
+        updateNav();
+      } else if (breakWidths.length > 0 && availableSpace > breakWidths[breakWidths.length - 1]) {
+        $hlinks.children().first().detach().appendTo($vlinks);
+        breakWidths.pop();
+        updateNav();
+      }
+
+      if ($hlinks.children().length > 0) {
+        $btn.removeClass("hidden");
+      } else {
+        $btn.addClass("hidden");
+        closeHiddenLinks();
+      }
+    };
+
+    const scheduleUpdateNav = () => {
+      window.cancelAnimationFrame(rafId);
+      rafId = window.requestAnimationFrame(updateNav);
+    };
+
+    $btn.on("click", function () {
+      const willOpen = $hlinks.hasClass("hidden");
+      $hlinks.toggleClass("hidden");
+      $(this).attr("aria-expanded", willOpen ? "true" : "false");
+    });
+
+    $hlinks.on("click", "a", function () {
+      closeHiddenLinks();
+    });
+
+    $(document).on("click", function (event) {
+      if (!$siteNav.is(event.target) && $siteNav.has(event.target).length === 0) {
+        closeHiddenLinks();
+      }
+    });
+
+    $(window).on("resize", function () {
+      closeHiddenLinks();
+      scheduleUpdateNav();
+    });
+
+    $(window).on("load", scheduleUpdateNav);
+    scheduleUpdateNav();
+  }
 });
